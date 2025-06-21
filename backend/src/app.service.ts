6917b54a0cceca2ca38cf604e244855c59734602
Angelo Marcos <64624199+AngeloMarcos@users.noreply.github.com>
2025-06-20 22:31:36 -0300

    Remove rotas duplicadas de usuarios

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,23 +1,8 @@
-// backend/src/app.service.ts
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
-import { CreateUserDto } from './users/dto/create-user.dto';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  constructor(
-    @InjectRepository(User)
-    private usersRepo: Repository<User>,
-  ) {}
-
-  findAll(): Promise<User[]> {
-    return this.usersRepo.find();
-  }
-
-  create(data: CreateUserDto): Promise<User> {
-    const user = this.usersRepo.create(data);  // já faz new User() + assign
-    return this.usersRepo.save(user);
+  getHello(): string {
+    return "API rodando";
   }
 }
